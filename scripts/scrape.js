b576abe6e1a1a3854f80fabe149669c8cd4bc729
DaniGreek <d.greco6@studenti.unibg.it>
2023-02-24 00:07:15 +0100

    inserting promises

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,9 +1,11 @@
 const cheerio = require('cheerio');
-const fs = require('fs');
 
-const urlBikester = 'https://www.bikester.it/biciclette/bici-elettriche/'
+const urlBikester = 'https://www.bikester.it/biciclette/bici-elettriche/';
+const urlProducts = 'https://www.bikester.it';
+
 let arrayUrl = [];
 let maxPage;
+let maxProduct;
 
 fetch(urlBikester)
     .then(function(response) {
@@ -13,13 +15,16 @@ fetch(urlBikester)
         console.log('fetch successfull - cheerio parsing...');
         const txt = cheerio.load(text);
         maxPage = getMaxPage(txt);
+        maxProduct = getMaxProduct(txt);
+        console.log('number of products found on page: ' + maxProduct);
         console.log(`pages to parse: ${maxPage}`);
         constructArrayUrl(txt);
     })
-    .catch(function(err) {  
-        console.log(err);  
+    .catch(function(err) {
+        console.log(err);
     });
 
+// gets the number of pages where there are products
 function getMaxPage($){
     const products = $('.pagination__listitem').children('a');
     const p = products.map((i, x) => $(x).text()).toArray();
@@ -33,13 +38,23 @@ function getMaxPage($){
     return Math.max.apply(null, num);
 }
 
-function constructArrayUrl($){
+// gets number of products
+function getMaxProduct($){
+    const num = $('.scroll-pivot').children('.cyc-flex.cyc-flex--end.cyc-flex--wrap.cyc-margin_bottom-5.contentwrapper').children('span');
+    const n = +num.attr('data-value');
+    return n;
+}
+
+// creates array of product urls 
+function constructArrayUrl(){
     let arrayUrlPages = [];
     arrayUrlPages.push(urlBikester);
     for(let i=2; i<=maxPage; i++){
         arrayUrlPages.push(urlBikester + '?page=' + i + '&sz=48');
     };
-    arrayUrlPages.forEach(function(url, i){
+    let fetches = [];
+    arrayUrlPages.forEach(function(url, i, array){
+        fetches.push(
         fetch(url)
             .then(function(response) {
                 return response.text();
@@ -49,13 +64,43 @@ function constructArrayUrl($){
                 parseTxt(cheerio.load(text));
             })
             .catch(function(err) {  
-                console.log(err);  
-            });
+                console.log(err);
+            })
+        );
+    });
+    // wait for all fetches to complete before proceeding
+    Promise.all(fetches).then(function(){
+        console.log('\nfound ' + arrayUrl.length + ' products to scrape\nscraping phase initiating...');
+        getData();
     })
 }
 
+// gets urls of single products in pages
 function parseTxt($){
     const products = $('.product-tile-inner').children('.is-relative').children('a');
     const p = products.map((i, x) => $(x).attr('href')).toArray();
     arrayUrl.push.apply(arrayUrl, p);
+}
+
+function getData() {
+    let fetches = [];
+    //arrayUrl.forEach(function(url, i, array){
+    //    fetches.push(
+        fetch('https://www.bikester.it/ortler-bozen-trapeze-M1051259.html?vgid=G1550450#cgid=38448')//urlProducts + url
+            .then(function(response) {
+                return response.text();
+            })
+            .then(function(text) {
+                parserData(cheerio.load(text));
+            })
+            .catch(function(err) {  
+                console.log(err);
+            })
+        //);
+    //});
+}
+
+parserData($){
+    //const price = $()
+    //const tab = 
 }
